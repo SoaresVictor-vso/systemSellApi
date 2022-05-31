51615590e9a42937f9335e9993c06e0ec288879c
SoaresVictor-vso <soaresvictor479@gmail.com>
2022-05-31 13:40:16 -0300

    reconfig port

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { setOperation } = require('./reqController');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 
